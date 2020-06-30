15e389eace96f0cc5aa198ca716387a0b0690b70
msdonoghue <49663739+msdonoghue@users.noreply.github.com>
2020-06-30 14:21:08 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    while (true) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(100)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(100)
+    }
+})
+for (let index = 0; index < 2; index++) {
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    basic.pause(1000)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    basic.pause(100)
+}
